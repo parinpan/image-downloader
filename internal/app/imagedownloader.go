d383aa3298bf9464bc920470d83773fbcf0526c4
Fachrin Aulia <fachrinfan@gmail.com>
2023-08-13 00:23:44 +0200

    add downloader client connection pool and timeout

--- internal/app/imagedownloader.go
+++ internal/app/imagedownloader.go
@@ -15,6 +15,10 @@ import (
 	imageDownloaderPkg "fachr.in/image-downloader/pkg/imagedownloader"
 )
 
+const (
+	unlimited = 0
+)
+
 func StartImageDownloaderApp(ctx context.Context) error {
 	imageDownloader := &imagedownloader.ImageDownloader{
 		FixtureLoader: &fixture.Fixture{
@@ -23,7 +27,15 @@ func StartImageDownloaderApp(ctx context.Context) error {
 		},
 		DownloaderClient: &imageDownloaderPkg.Client{
 			HTTPClient: &imageDownloaderPkg.HTTPClient{
-				BaseClient: http.DefaultClient,
+				BaseClient: &http.Client{
+					Transport: &http.Transport{
+						MaxIdleConns:        250,
+						MaxIdleConnsPerHost: 25,
+						MaxConnsPerHost:     unlimited,
+						IdleConnTimeout:     unlimited,
+					},
+					Timeout: time.Duration(60) * time.Second,
+				},
 				RetryOption: imageDownloaderPkg.RetryOption{
 					BaseDelay:   time.Duration(50) * time.Millisecond,
 					MaxDelay:    time.Duration(3) * time.Second,
