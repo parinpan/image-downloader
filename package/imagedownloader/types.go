eabe36d8d59aed8ddfce53944317ba2e7f7f6925
Fachrin Aulia <fachrinfan@gmail.com>
2023-08-12 19:37:59 +0200

    add common image extension list

---
+++ package/imagedownloader/types.go
@@ -0,0 +1,16 @@
+package imagedownloader
+
+var (
+	CommonImageContentTypeExtensions = map[string]string{
+		"image/jpeg":         ".jpg",
+		"image/png":          ".png",
+		"image/gif":          ".gif",
+		"image/bmp":          ".bmp",
+		"image/webp":         ".webp",
+		"image/svg+xml":      ".svg",
+		"image/x-icon":       ".ico",
+		"image/tiff":         ".tiff",
+		"image/vnd.radiance": ".hdr",
+		"image/jp2":          ".jp2",
+	}
+)
