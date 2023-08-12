5d43d531fb3ae64ac3f9af8b7ee845bd318997d4
Fachrin Aulia <fachrinfan@gmail.com>
2023-08-12 19:07:38 +0200

    add image downloader http client + unit tests + mock

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module fachr.in/image-downloader
 
 go 1.20
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	go.uber.org/mock v0.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- package/imagedownloader/client.go
+++ package/imagedownloader/client.go
@@ -1,1 +1,73 @@
 package imagedownloader
+
+import (
+	"context"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+)
+
+const (
+	contentTypeHeaderKey = "Content-Type"
+)
+
+var (
+	ErrMakeRequest   = errors.New("could not build http request")
+	ErrFetchResponse = errors.New("could not fetch http response")
+	ErrImageNotFound = errors.New("could not download a non-existing image")
+	ErrInvalidImage  = errors.New("could not download an invalid image")
+	ErrOpenImageFile = errors.New("could not create a new image file")
+	ErrCopyImage     = errors.New("could not copy image into the destination path")
+)
+
+type httpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type Client struct {
+	HTTPClient   httpClient
+	CreateFileFn func(name string) (*os.File, error)
+	CopyFileFn   func(dst io.Writer, src io.Reader) (written int64, err error)
+}
+
+func (c *Client) DownloadImage(ctx context.Context, url string, destinationPath func(contentType string) string) error {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return errors.Join(ErrMakeRequest, err)
+	}
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return errors.Join(ErrFetchResponse, err)
+	}
+
+	// close body in every call made
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNotFound {
+		return ErrImageNotFound
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return ErrInvalidImage
+	}
+
+	return c.saveImage(resp.Body, destinationPath(resp.Header.Get(contentTypeHeaderKey)))
+}
+
+func (c *Client) saveImage(body io.ReadCloser, destinationPath string) error {
+	file, err := c.CreateFileFn(destinationPath)
+	if err != nil {
+		return errors.Join(ErrOpenImageFile, err)
+	}
+
+	// close file whenever opened
+	defer file.Close()
+
+	if _, err := c.CopyFileFn(file, body); err != nil {
+		return errors.Join(ErrCopyImage, err)
+	}
+
+	return nil
+}
